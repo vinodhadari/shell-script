d05f04ff9c4f175c1cf22d254bb3e5927781afe1
vinodhadar <vinod.adari121@gmail.com>
2025-09-24 13:14:19 +0530

    variable 04

---
+++ 04-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Arguments
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on Zoom Webinar"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
